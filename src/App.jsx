d7782bbb85116d1f1a5f4c09cce294dbc4f79c2e
Mica <micpercara@gmail.com>
2025-10-16 16:48:40 -0300

    corrección deploy para github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom'
 import Home from './paginas/Home';
 import ListaPokemones from './paginas/ListaPokemones';
 import DetallesPokemones from './paginas/DetallesPokemones';
@@ -7,13 +7,11 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/listado" element={<ListaPokemones />} />
         <Route path="/detalle/:id" element={<DetallesPokemones />} />
       </Routes>
-    </Router>
   );
 }
 
