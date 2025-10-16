c245ce555f19fb7cf4ff114c4e44bb985c5832a8
Mica <micpercara@gmail.com>
2025-10-16 16:31:10 -0300

    Deploy en GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
